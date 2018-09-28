4eeea63b62527d7c8bf4ebe4c507950394f7660f
Emba Moussa <emba.moussa@eventinc.de>
2018-09-28 22:59:07 +0200

    feat: rename css classnames

--- client/src/components/Layer.js
+++ client/src/components/Layer.js
@@ -3,13 +3,13 @@ import logo from '../assets/logo.svg';
 import '../styles/Layer.css';
 import MoviesContainer from './MoviesContainer';
 
-class App extends Component {
+class Layer extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Movie DB</h1>
+      <div className="movie-app">
+        <header className="movie-app-header">
+          <img src={logo} className="movie-app-logo" alt="logo" />
+          <h1 className="movie-app-title">Movie DB</h1>
         </header>
         <MoviesContainer />
       </div>
@@ -17,4 +17,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Layer;
