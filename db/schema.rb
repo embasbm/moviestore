681cff399c1b3c5e546c108b9751f18ed1739da7
Emba Moussa <emba.moussa@eventinc.de>
2018-10-01 22:36:13 +0200

    feat: remove password field from suer, will eventually get it back, when dealing with authentication

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2018_10_01_004005) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
