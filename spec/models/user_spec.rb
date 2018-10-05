4deca454f2af78e7312f241dbdde70efed23de00
Emba Moussa <emba.moussa@eventinc.de>
2018-10-05 22:18:42 +0200

    refa: fix rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,25 +5,21 @@ RSpec.describe User, type: :model do
     it 'fails on empty email' do
       record = User.new
       record.email = ''
-      record.password = ''
       record.valid?
       expect(record.errors[:email]).to include("can't be blank")
-      expect(record.errors[:password]).to include("can't be blank")
     end
 
     it 'success when email present' do
       record = User.new
       record.email = Faker::Internet.email
-      record.password = Faker::Internet.password
       record.valid?
       expect(record.errors[:email]).not_to include("can't be blank")
     end
 
     it 'fails on empty email' do
       email = Faker::Internet.email
-      password = Faker::Internet.password
-      record_a = User.create(email: email, password: password)
-      record_b = User.new(email: email, password: password)
+      record_a = User.create(email: email)
+      record_b = User.new(email: email)
       record_b.valid?
       expect(record_b.errors[:email]).to include("has already been taken")
     end
